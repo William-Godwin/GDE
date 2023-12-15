52746e764ab3f5ba4c5d5185d100db239ec1351d
William Godwin <anton.pernatj@yandex.ru>
2023-12-15 16:50:25 +0300

    del_config

--- cmake/entt-config.cmake
+++ cmake/entt-config.cmake
@@ -9,12 +9,10 @@ if(NOT EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/extern/entt")
     message(NOTICE "entt bingings source not found")
     message(NOTICE "initializing/updating the entt submodule...")
 
-    add_subdirectory(extern/entt)
-
     # update the c++ bingings submodule to populate it with
     # the necessary source for the entt library
     execute_process(
-        COMMAND git submodule update --init --recursive extern/entt
+        COMMAND git submodule update --init extern/entt
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         COMMAND_ERROR_IS_FATAL ANY
     )
